d2df1d02faf4e21ce4a4dd434b32d3fdeb13c905
Nguyễn Văn Liêm - 60135923 <nvliemc4ncthanh@gmail.com>
2022-02-23 17:10:08 +0700

    Create layer

---
+++ eShopSolution.Application/Catalog/Products/IManageProductService.cs
@@ -0,0 +1,23 @@
+﻿using eShopSolution.Application.Catalog.Products.Dtos;
+using eShopSolution.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    public interface IManageProductService
+    {
+        Task<int> Create(ProductCreateRequest request);
+
+        Task<int> Update(ProductEditRequest request);
+
+        Task<int> Delete(int productId);
+
+        Task<List<ProductViewModel>> getAll();
+
+        Task<PagedViewModel<ProductViewModel>> getAllPaging(string keyword, int pageIndex, pageSize);
+
+    }
+}
