d83450c9082a14ed80ea6b32b987b80dcbfa105b
Nguyễn Văn Liêm - 60135923 <nvliemc4ncthanh@gmail.com>
2022-04-12 09:41:17 +0700

    Sign in with google

---
+++ eShopSolution.TestApp/Services/EmailSenderService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace eShopSolution.TestApp.Services
+{
+    public class EmailSenderService : IEmailSender
+    {
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+
+        }
+    }
+}
