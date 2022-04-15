dfad0de598ed78896ac5eaf18ab95f1ceffeddfa
Nguyễn Văn Liêm - 60135923 <nvliemc4ncthanh@gmail.com>
2022-04-15 16:37:29 +0700

    Complete filter product using dropdown item

---
+++ eShopSolution.AdminApp/Services/CategoryApiClient.cs
@@ -0,0 +1,25 @@
+﻿using eShopSolution.ViewModels.Catalog.Categories;
+using eShopSolution.ViewModels.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace eShopSolution.AdminApp.Services
+{
+    public class CategoryApiClient : BaseApiClient, ICategoryApiClient
+    {
+        public CategoryApiClient(IHttpClientFactory httpClientFactory, 
+            IConfiguration configuration, 
+            IHttpContextAccessor httpContextAccessor) 
+            : base(httpClientFactory, configuration, httpContextAccessor)
+        {
+        }
+
+        public async Task<List<CategoryVm>> GetAll(string languageId)
+        {
+            return await GetListAsync<CategoryVm>("/api/categories?languageId="+languageId);
+        }
+    }
+}
